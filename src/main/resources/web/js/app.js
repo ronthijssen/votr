9a8e933451fe7710dabad95c5003689e31c61d8a
Ron Thijssen <ron@jpoint.nl>
2014-02-04 11:55:10 +0100

    app named votr

--- src/main/resources/web/js/app.js
+++ src/main/resources/web/js/app.js
@@ -2,7 +2,7 @@
 /*global angular */
 
 // Declare app level module which depends on filters, and services
-var myApp = angular.module('myApp', ['ngRoute']).
+var votr = angular.module('votr', ['ngRoute']).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/home.html' /* , controller: 'homeController' */});
 
